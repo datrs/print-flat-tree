dd92ad4d996623674050d5c3ea3db29420ed8b4d
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-23 14:37:39 +0100

    fix len

--- src/lib.rs
+++ src/lib.rs
@@ -43,10 +43,10 @@ extern crate structopt;
 
 use std::cmp;
 
-const VERT_CHAR: char = '│';
-const HORI_CHAR: char = '─';
-const LEFT_BOT_CHAR: char = '┐';
-const LEFT_TOP_CHAR: char = '┘';
+const DOWN: char = '│';
+const LEFT: char = '─';
+const TURN_DOWN: char = '┐';
+const TURN_UP: char = '┘';
 
 /// Configuration for the `print` function.
 #[derive(Debug, StructOpt)]
@@ -57,31 +57,39 @@ pub struct Options {
   pub verbose: bool,
   /// Nodes that are part of the flat-tree.
   #[structopt(help = "For example '0 1 2 3 7 8 9 10'")]
-  pub list: Vec<i32>,
+  pub tree: Vec<usize>,
 }
 
 /// Converts a `flat_tree` to a string.
 pub fn fmt(opts: &Options) {
-  let list = &opts.list;
-  let width = list.len() + 1;
+  let tree = &opts.tree;
 
+  // Fill a vec with bools, indicating if a value exists or not.
+  let max = tree.iter().fold(0, |prev, curr| cmp::max(prev, *curr));
+  let mut list = vec![false; max + 1];
+  for &i in tree {
+    list[i] = true;
+  }
+
+  let width = list.len() + 1;
   let last_block = list.len() - list.len() % 2;
   let _roots = flat_tree::full_roots(last_block as u64);
 
-  let max = list.iter().fold(0, |prev, curr| cmp::max(prev, *curr));
   let blank = format!("{:width$}", ' ', width = width + 1);
   let mut matrix = vec![vec![blank.to_string(); max as usize]; list.len()];
 
+  println!("length: {:?}", list.len());
   for i in 0..list.len() {
     let depth = flat_tree::depth(i as u64);
     let val = format!("{:width$}", i, width = width + 1);
     matrix[i as usize][depth as usize] = val;
 
     if let Some(children) = flat_tree::children(i as u64) {
-      add_path(list, &mut matrix, children.0, i as u64, depth, 1);
-      if (children.1 as usize) < list.len() {
-        add_path(list, &mut matrix, children.1, i as u64, depth, -1);
-      }
+      println!("i: {:?}, children: {:?}, depth: {:?}", i, children, depth);
+      add_path(&list, &mut matrix, children.0, i as u64, depth, 1);
+      // if (children.1 as usize) < list.len() {
+      //   add_path(list, &mut matrix, children.1, i as u64, depth, -1);
+      // }
     }
   }
 
@@ -89,36 +97,39 @@ pub fn fmt(opts: &Options) {
 }
 
 fn add_path(
-  list: &[i32],
+  list: &[bool],
   matrix: &mut Vec<Vec<String>>,
   child: u64,
   parent: u64,
   parent_depth: u64,
   direction: i32,
 ) -> () {
-  if list.get(child as usize).is_some() {
-    let depth = flat_tree::depth(child);
-    let ptr = depth + 1;
+  if list.get(child as usize).is_none() {
+    return;
+  }
 
-    for i in ptr..parent_depth {
-      matrix[child as usize][i as usize] = pad(HORI_CHAR, HORI_CHAR);
-    }
-    let turn_char = if direction < 0 {
-      LEFT_TOP_CHAR
-    } else {
-      LEFT_BOT_CHAR
+  let depth = flat_tree::depth(child);
+  let ptr = depth + 1;
+
+  for i in ptr..parent_depth {
+    matrix[child as usize][i as usize] = pad(LEFT, LEFT);
+  }
+
+  let turn_char = if direction < 0 { TURN_UP } else { TURN_DOWN };
+  matrix[child as usize][ptr as usize] = pad(turn_char, LEFT);
+
+  let mut start: i32 = child as i32;
+  // println!(
+  //   "child {}, parent {}, direction {}",
+  //   start, parent, direction
+  // );
+  loop {
+    start += direction;
+    // println!("start:{:?} dir:{:?} parent:{:?}", start, direction, parent);
+    if start == parent as i32 {
+      break;
     };
-    matrix[child as usize][ptr as usize] = pad(turn_char, HORI_CHAR);
-
-    let mut start: i32 = child as i32;
-    loop {
-      start += direction;
-      println!("start:{:?} dir:{:?} parent:{:?}", start, direction, parent);
-      if start == parent as i32 {
-        break;
-      };
-      matrix[child as usize][ptr as usize] = pad(VERT_CHAR, ' ');
-    }
+    matrix[child as usize][ptr as usize] = pad(DOWN, ' ');
   }
 }
 
